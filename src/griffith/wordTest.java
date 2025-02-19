e5cbece7c9c94dd88fc884d12b3152948118b994
Mohammad Abrar Farooqui <mohammmadabrar.farooqui@student.griffith.ie>
2025-02-19 19:59:40 +0000

    add test class stub method for testContatins()

--- src/griffith/wordTest.java
+++ src/griffith/wordTest.java
@@ -6,9 +6,9 @@ import org.junit.jupiter.api.Test;
 
 class wordTest {
 
-	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
+	 @Test
+	    public void testContains() {
+	        // Stub test (to be implemented later)
+	    }
 
 }
