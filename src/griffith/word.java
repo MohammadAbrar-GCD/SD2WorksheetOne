9a560525fde55348f7f359dce29b22cd65648f1c
Mohammad Abrar Farooqui <mohammmadabrar.farooqui@student.griffith.ie>
2025-02-19 19:58:19 +0000

    Add construct & stub method

--- src/griffith/word.java
+++ src/griffith/word.java
@@ -2,4 +2,26 @@ package griffith;
 
 public class word {
 
+	private char[] letters;
+
+    // Constructor
+    public word(char[] letters) {
+        this.letters = letters;
+    }
+
+    // Stub method to check if the word contains a character
+    public boolean contains(char symbol) {
+        return false; // Stub implementation
+    }
+
+    // Stub method to return the length of the word
+    public int length() {
+        return 0; // Stub implementation
+    }
+
+    // Method to return the letters array
+    public char[] getLetters() {
+        return letters;
+    }
 }
+
